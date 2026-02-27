c19cd9721834bd633227ce745a7ad975119162b7
Toni <tonidujic19@gmail.com>
2026-02-28 00:30:25 +0200

    Refactor: move auth logic to authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,34 @@
 const jwt = require("jsonwebtoken");
+const { getDB } = require("../db/mongo");
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+let token;
+
+const sendToken = (user, req, res) => {
+  token = signToken(user._id);
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    maxAge: 60 * 60 * 1000,
+  });
+
+  user.password = undefined;
+
+  res.status(200).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
 
 exports.protect = (req, res, next) => {
-  console.log("cookie header:", req.headers.cookie);
-  console.log("cookies obj:", req.cookies);
-
-  const token = req.cookies?.jwt;
+  token = req.cookies?.jwt;
 
   if (!token) {
     return res.status(401).json({ message: "You are not logged in" });
@@ -22,4 +46,46 @@ exports.protect = (req, res, next) => {
 
 exports.protectedInfo = (req, res) => {
   res.status(200).json({ message: "Info" });
+};
+
+exports.signUp = async (req, res) => {
+  const { username, password } = req.body;
+
+  const db = getDB();
+  if (!db) return res.status(500).json({ message: "DB not connected" });
+  const users = db.collection("users");
+
+  const user = await users.insertOne({ username, password });
+
+  if (user) sendToken({ _id: user.insertedId, username }, req, res);
+  else {
+    return res.status(400).json({
+      message: "Error",
+    });
+  }
+};
+
+exports.logIn = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({
+      message: "Missing username and password",
+    });
+  }
+  const db = getDB();
+  const users = db.collection("users");
+
+  const user = await users.findOne({
+    username,
+    password,
+  });
+
+  if (!user) {
+    return res.status(400).json({
+      message: "Your username or password are incorrect",
+    });
+  } else {
+    return sendToken(user, req, res);
+  }
 };
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,75 +1,9 @@
 const express = require("express");
-const { getDB } = require("../db/mongo");
 const router = express.Router();
-const jwt = require("jsonwebtoken");
+const authController = require("../controllers/authController.js");
 
-//token sign
-const signToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
-};
+router.post("/signup", authController.signUp);
 
-let token;
-//sending signed token
-const sendToken = (user, req, res) => {
-  token = signToken(user._id);
-  res.cookie("jwt", token, {
-    httpOnly: true,
-    maxAge: 60 * 60 * 1000,
-  });
-
-  user.password = undefined;
-
-  res.status(200).json({
-    status: "success",
-    token,
-    data: {
-      user,
-    },
-  });
-};
-
-router.post("/signup", async (req, res) => {
-  const { username, password } = req.body;
-
-  const db = getDB();
-  if (!db) return res.status(500).json({ message: "DB not connected" });
-  const users = db.collection("users");
-
-  const user = await users.insertOne({ username, password });
-
-  if (user) sendToken({ _id: user.insertedId, username }, req, res);
-  else {
-    return res.status(400).json({
-      message: "Error",
-    });
-  }
-});
-
-router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-
-  if (!username || !password) {
-    return res.status(400).json({
-      message: "Missing username and password",
-    });
-  }
-  const db = getDB();
-  const users = db.collection("users");
-
-  const user = await users.findOne({
-    username,
-    password,
-  });
-
-  if (!user) {
-    return res.status(400).json({
-      message: "Your username or password are incorrect",
-    });
-  } else {
-    return sendToken(user, req, res);
-  }
-});
+router.post("/login", authController.logIn);
 
 module.exports = router;
