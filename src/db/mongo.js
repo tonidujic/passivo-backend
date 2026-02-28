60165f820df2562dc20b61ca5d205297d4288d9d
Toni <tonidujic19@gmail.com>
2026-03-01 01:46:52 +0200

    Remove debug code

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -2,25 +2,12 @@ const { MongoClient } = require("mongodb");
 
 const client = new MongoClient(process.env.MONGO_URI);
 let db;
-// exports.connectDB = async () => {
-//   try {
-//     console.log("MONGO_URI exists?", !!process.env.MONGO_URI);
-//     await client.connect();
-//     db = client.db("passivo_db");
-//   } catch (err) {
-//     console.log(err.message);
-//   }
-// };
-
 exports.connectDB = async () => {
   try {
-    console.log("➡️ Connecting to Mongo...");
     await client.connect();
-    console.log("✅ Connected to Mongo!");
     db = client.db("passivo_db");
   } catch (err) {
-    console.log("❌ Mongo connect error:", err);
-    throw err; // važno da app.js vidi da je failalo
+    console.log(err.message);
   }
 };
 
